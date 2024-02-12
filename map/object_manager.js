0859b84007274c411dfe6e1710ca0cf100d0e580
hrodna26 <159182009+hrodna26@users.noreply.github.com>
2024-02-12 23:44:37 +0300

    Add files via upload

--- map/object_manager.js
+++ map/object_manager.js
@@ -6,15 +6,11 @@ function init () {
             zoom: 12
         }),
         objectManager = new ymaps.ObjectManager({
-            // Чтобы метки начали кластеризоваться, выставляем опцию.
             clusterize: true,
-            // ObjectManager принимает те же опции, что и кластеризатор.
             gridSize: 32,
             clusterDisableClickZoom: true
         });
 
-    // Чтобы задать опции одиночным объектам и кластерам,
-    // обратимся к дочерним коллекциям ObjectManager.
     objectManager.objects.options.set('preset', 'islands#redDotIcon');
     objectManager.clusters.options.set('preset', 'islands#redClusterIcons');
     myMap.geoObjects.add(objectManager);
